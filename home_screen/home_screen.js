6ab9c058af847d9e66ff0b7e562a7226f8ad210e
shinoyumi910 <shinoyumi554745@gmail.com>
2026-06-18 11:58:44 +0900

    ボタンの調整

--- home_screen/home_screen.js
+++ home_screen/home_screen.js
@@ -1,3 +1,19 @@
+/*
+//firebaseとの接続
+initializeApp()
+getFirestore()
+
+// kadai1から取得
+const q = query(
+    collection(db, "kadai1"),
+    orderBy("created", "desc")
+);
+
+// 未完了だけ取得
+t.done === false
+*/
+
+//ユーザが選択した画像を表示する
 const imageInput = document.getElementById("imageInput");
 const oshiImage = document.getElementById("oshiImage");
 
